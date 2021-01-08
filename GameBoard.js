9e04aa66550ead21f96f30b18ca35b4928e675b6
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-08 09:16:07 -0700

    create showGameStatus method

--- GameBoard.js
+++ GameBoard.js
@@ -1,1 +1,16 @@
 import { GRID_SIZE, CELL_SIZE, OBJECT_TYPE, CLASS_LIST } from "./setup.js";
+
+class GameBoard {
+  constructor(DOMGrid) {
+    this.dotCount = 0;
+    this.grid = [];
+    this.DOMGrid = DOMGrid;
+  }
+
+  showGameStatus(gameWin) {
+    const div = document.createElement("div");
+    div.classList.add("game-status");
+    div.innerHTML = `${gameWin ? "WIN!" : "GAME OVER"}`;
+    this.DOMGrid.appendChild(div);
+  }
+}
