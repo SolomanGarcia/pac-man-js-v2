23a16206627a71d4450b6d00e16ece14668fdfbc
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-08 09:30:31 -0700

    create more methods on GameBoard class

--- GameBoard.js
+++ GameBoard.js
@@ -38,4 +38,12 @@ class GameBoard {
   removeObject(pos, classes) {
     this.grid[pos].classList.remove(...classes);
   }
+
+  objectExist(pos, object) {
+    return this.grid[pos].classList.contains(object);
+  }
+
+  rotateDiv(pos, deg) {
+    this.grid[pos].style.transform = `rotate(${deg}deg)`;
+  }
 }
