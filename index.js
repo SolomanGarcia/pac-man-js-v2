0a9dc981933b89aebcddfd347518c2a059cf62e1
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-08 08:53:53 -0700

    add DOM elements to index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Working!");
+import { LEVEL, OBJECT_TYPE } from "./setup.js";
+
+// DOM Elements
+const gameGrid = document.querySelector("#game");
+const scoreTable = document.querySelector("#score");
+const startButton = document.querySelector("#start-button");
+
+// Game Constants
