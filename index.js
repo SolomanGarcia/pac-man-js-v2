e101d6b1a644b762c005413c866014a40bf12de8
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-08 18:47:35 -0700

    work on deploying

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { LEVEL, OBJECT_TYPE } from "./setup";
-import { randomMovement } from "./ghostmoves";
+import { LEVEL, OBJECT_TYPE } from "./setup.js";
+import { randomMovement } from "./ghostmoves,js";
 // classes
 import GameBoard from "./GameBoard.js";
 import Pacman from "./Pacman.js";
